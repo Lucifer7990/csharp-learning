2b4e6eb3b75d9815eafb35ab178632378bc7198b
Lucifer7990 <darjidhruv720@gmail.com>
2025-09-10 19:19:35 +0530

    In Marcove class add new method for converting text to array of words.

--- Markov.cs
+++ Markov.cs
@@ -1,14 +1,64 @@
 using System.Text;
 class Markov
 {
+
     static void Main()
+    {
+        string all1 = File.ReadAllText(@".\datafiles\indian_history.txt", Encoding.UTF8);
+        string all2 = File.ReadAllText(@".\datafiles\New Text Document.txt", Encoding.UTF8);
+        string all3 = File.ReadAllText(@".\datafiles\New Text Document (2).txt", Encoding.UTF8);
+        //string str = "abcdefghijklmnopqrstuvwxyz";
+
+        string str = all1 + all3 + all2;
+        str = str.ToLower();
+        str = str.Replace("–", " ");
+        str = str.Replace("-", " ");
+        str = str.Replace("#", " ");
+        str = str.Replace("*", " ");
+        str = str.Replace(";", " ");
+        str = str.Replace("\"", " ");
+        str = str.Replace("=", " ");
+        str = str.Replace("\n", " ");
+        str = str.Replace("\r", " ");
+        str = str.Replace("\t", " ");
+        str = str.Replace("  ", " ");
+        str = str.Replace("  ", " ");
+        str = str.Replace("  ", " ");
+        str = str.Replace("  ", " ");
+
+        foreach (var a in str)
+        {
+            int b = a;
+            if ((b >= 97 && b <= 122) || a == ' ')
+            {
+
+            }
+            else
+            {
+                str = str.Replace(a.ToString(), "");
+            }
+
+        }
+        str = str.Replace("  ", " "); str = str.Replace("  ", " "); str = str.Replace("  ", " ");
+
+        string[] arr = str.Split(" ");
+
+        foreach (var a in arr)
+        {
+            if(a.Length==2)
+                Console.Write(a + " ");
+        }
+
+
+    }
+    static void VoWCount()
     {
 
         string all1 = File.ReadAllText(@".\datafiles\indian_history.txt", Encoding.UTF8);
         string all2 = File.ReadAllText(@".\datafiles\New Text Document.txt", Encoding.UTF8);
         string all3 = File.ReadAllText(@".\datafiles\New Text Document (2).txt", Encoding.UTF8);
         //string str = "abcdefghijklmnopqrstuvwxyz";
-        string str = all1+all3;
+        string str = all1 + all3 + all2;
 
 
         int Count = 0;
@@ -24,7 +74,7 @@ class Markov
             int b = a;
             if (b >= 97 && b <= 122)
             {
-                Console.Write(a);
+                //Console.Write(a);
                 Count++;
                 if (b == 97 || b == 101 || b == 105 || b == 111 || b == 117)
                 {
@@ -38,9 +88,6 @@ class Markov
         Console.WriteLine(per);
         Console.WriteLine(vovlCount);
         Console.WriteLine(Count);
-
-
-
 
     }
 }
--- obj/Debug/net8.0/temp.AssemblyInfo.cs
+++ obj/Debug/net8.0/temp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f19e5f344b92007db09be5141e46f73269985bf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c79cd82d1026b4941a0ebced2d8c70bdd7a7ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
